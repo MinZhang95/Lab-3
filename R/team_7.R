77d3ad91ec0b06da420ad54ed6db9737a66c4f74
Stephanie <srein@iastate.edu>
2019-03-22 15:39:49 -0500

    Moved gadm36_AUS_shp files to inst/extdata folder per instructions when checking the package

--- R/team_7.R
+++ R/team_7.R
@@ -2,21 +2,21 @@
 #' @aliases team_7
 #' @title  team_7
 #' @usage team_7(file,tolerance)
-#' @import magrittr
+#' @import dplyr maptools purrr sf
+#' @importFrom "methods" "as"
 #' @param file A path to a shape file
 #' @param tolerance The value used to thin the polygon
 #' @return A data frame of the geometry file \code{file}
 #' @examples
-#' tolerance <- 0.1
-#' file <- "./data/gadm36_AUS_shp/gadm36_AUS_1.shp"
-#' team_7(file,tolerance)
+#' fpath <- system.file("extdata","gadm36_AUS_1.shp",package="Lab3R")
+#' team_7(fpath,0.1)
 #' @export
 
 team_7 <- function(file, tolerance){
   
-  ozbig <- sf::read_sf(file)
-  oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE)
-  oz <- sf::st_as_sf(oz_st)
+  ozbig <- read_sf(file)
+  oz_st <- thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE)
+  oz <- st_as_sf(oz_st)
   
   f <- function(dframe){
     dframe <- data.frame(order = c(1:nrow(dframe)), long = dframe$x, lat = dframe$y)
@@ -24,10 +24,10 @@ team_7 <- function(file, tolerance){
   
   ## Here our depth is 3, but that could change depending on the file
   ozplus <- oz$geometry %>% 
-    purrr::modify_depth(3,data.frame) %>% 
-    purrr::modify_depth(3,f) %>%
-    purrr::flatten() %>%
-    purrr::flatten() %>%
+    modify_depth(3,data.frame) %>% 
+    modify_depth(3,f) %>%
+    flatten() %>%
+    flatten() %>%
     bind_rows(.id = "group")
   
   return(ozplus)
