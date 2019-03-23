a1a5ec10b495ac3105e503cac28798fb37a5fab0
Stephanie <srein@iastate.edu>
2019-03-23 16:23:25 -0500

    test-team_7.R file

--- tests/testthat/test-team_7.R
+++ tests/testthat/test-team_7.R
@@ -3,4 +3,5 @@ context("test-team_7")
 test_that("creating dataframe for a country's map works", {
   expect_warning(team_7(file = "bla"), "file does not exist")
   expect_warning(team_7(tolerance = "bla"), "tolerance is not numeric")
+  expect_equal(is.data.frame(team_7(tolerance = 0.1)), TRUE)
 })
