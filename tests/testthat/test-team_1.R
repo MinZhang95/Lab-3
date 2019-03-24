c17dff09cd4ff5595ce85df6bd83e20f3f3d6e52
Min Zhang <minz@iastate.edu>
2019-03-23 23:45:40 -0500

    update team_1 function with more info

--- tests/testthat/test-team_1.R
+++ tests/testthat/test-team_1.R
@@ -3,5 +3,6 @@ context("test-team_1")
 test_that("creating dataframe for a country's map works", {
   expect_warning(team_1(file = "bla"), "file does not exist")
   expect_warning(team_1(tolerance = "bla"), "tolerance is not numeric")
+  expect_warning(team_1(tolerance = -1), "tolerance must be a positive number")
   expect_equal(is.data.frame(team_1(tolerance = 0.1)), TRUE)
 })
